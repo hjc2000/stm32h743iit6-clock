8e92ebab58fd1b5248620ffc6563ac55d71fb6b1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-10 00:38:45 +0800

    HseClock

--- private_src/HseClock.cpp
+++ private_src/HseClock.cpp
@@ -1,7 +1,4 @@
 #include "HseClock.h"
-#include <base/di/SingletonGetter.h>
-#include <bsp-interface/di/interrupt.h>
-#include <hal.h>
 
 bsp::HseClock &bsp::HseClock::Instance()
 {
@@ -34,13 +31,8 @@ std::string bsp::HseClock::Name() const
     return "hse";
 }
 
-void bsp::HseClock::TurnOn(base::Hz const &crystal_frequency,
-                           int32_t input_division,
-                           int32_t output_division)
+void bsp::HseClock::Open(base::Hz const &crystal_frequency)
 {
-    _frequency = crystal_frequency;
-    _state = IClockSource_State::On;
-
     RCC_OscInitTypeDef def{};
     def.OscillatorType = RCC_OSCILLATORTYPE_HSE;
     def.HSEState = RCC_HSE_ON;
@@ -49,35 +41,13 @@ void bsp::HseClock::TurnOn(base::Hz const &crystal_frequency,
     {
         throw std::runtime_error{"打开 hse 时钟源失败。"};
     }
+
+    _frequency = crystal_frequency;
+    _state = IClockSource_State::On;
 }
 
-void bsp::HseClock::TurnOn(int32_t input_division, int32_t output_division)
+void bsp::HseClock::SetAsBypass(base::Hz external_clock_frequency)
 {
-    throw std::runtime_error{"hse 使用外部晶振，必须设置晶振频率，必须调用正确的 TurnOn 重载。"};
-}
-
-void bsp::HseClock::TurnOff()
-{
-    _state = IClockSource_State::Off;
-
-    RCC_OscInitTypeDef def{};
-    def.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    def.HSEState = RCC_HSE_OFF;
-
-    HAL_StatusTypeDef result = HAL_RCC_OscConfig(&def);
-    if (result != HAL_StatusTypeDef::HAL_OK)
-    {
-        throw std::runtime_error{"关闭 hse 时钟源失败。"};
-    }
-}
-
-void bsp::HseClock::SetAsBypass(base::Hz external_clock_frequency,
-                                int32_t input_division,
-                                int32_t output_division)
-{
-    _frequency = external_clock_frequency;
-    _state = IClockSource_State::Bypass;
-
     RCC_OscInitTypeDef def{};
     def.OscillatorType = RCC_OSCILLATORTYPE_HSE;
     def.HSEState = RCC_HSE_BYPASS;
@@ -87,6 +57,25 @@ void bsp::HseClock::SetAsBypass(base::Hz external_clock_frequency,
     {
         throw std::runtime_error{"设置 hse 时钟源为旁路失败。"};
     }
+
+    _frequency = external_clock_frequency;
+    _state = IClockSource_State::Bypass;
+}
+
+void bsp::HseClock::Close()
+{
+    RCC_OscInitTypeDef def{};
+    def.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    def.HSEState = RCC_HSE_OFF;
+
+    HAL_StatusTypeDef result = HAL_RCC_OscConfig(&def);
+    if (result != HAL_StatusTypeDef::HAL_OK)
+    {
+        throw std::runtime_error{"关闭 hse 时钟源失败。"};
+    }
+
+    _frequency = 0;
+    _state = IClockSource_State::Off;
 }
 
 bsp::IClockSource_State bsp::HseClock::State() const
--- private_src/HseClock.h
+++ private_src/HseClock.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <base/define.h>
+#include <base/di/SingletonGetter.h>
 #include <bsp-interface/clock/IClockSource.h>
+#include <bsp-interface/di/interrupt.h>
+#include <hal.h>
 
 namespace bsp
 {
+    /// @brief hse 时钟源。需要外部晶振。
     class HseClock :
         public bsp::IClockSource
     {
@@ -20,31 +24,17 @@ namespace bsp
         std::string Name() const override;
 
         /// @brief 打开时钟源。
-        /// @param crystal_frequency 有的时钟源需要外部晶振，且晶振允许的频率是一个范围，
-        /// 此时需要告诉本对象该晶振的频率才能打开本时钟源。这种情况下必须使用本 TurnOn 重载，
-        /// 否则会抛出异常。
-        /// @param input_division 输入分频系数。
-        /// @param output_division 输出分频系数。
-        void TurnOn(base::Hz const &crystal_frequency,
-                    int32_t input_division,
-                    int32_t output_division) override;
-
-        /// @brief 打开时钟源。
-        /// @param input_division 输入分频系数。
-        /// @param output_division 输出分频系数。
-        void TurnOn(int32_t input_division,
-                    int32_t output_division) override;
-
-        /// @brief 关闭时钟源。
-        void TurnOff() override;
+        /// @note 有的时钟源非常简单，外部振荡器输入后，直接就输出同频率的时钟信号，
+        /// 不支持分频和倍频。本重载用于这种简单的时钟源。
+        /// @param crystal_frequency
+        void Open(base::Hz const &crystal_frequency) override;
 
         /// @brief 设置为旁路。
         /// @param external_clock_frequency 外部时钟频率。
-        /// @param input_division 输入分频系数。
-        /// @param output_division 输出分频系数。
-        void SetAsBypass(base::Hz external_clock_frequency,
-                         int32_t input_division,
-                         int32_t output_division) override;
+        void SetAsBypass(base::Hz external_clock_frequency) override;
+
+        /// @brief 关闭时钟源。
+        void Close() override;
 
         /// @brief 本时钟源当前的状态。
         /// @return
