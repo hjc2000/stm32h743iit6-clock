aac76a24c53a86505ad7f8bf927dbc80bbc6b529
hjc2000 <hjc.666@foxmail.com>
2024-11-10 15:32:10 +0800

    base::IDictionary<std::string, bsp::IClockSource *> const &DI_ClockSourceCollection()

--- private_src/di/DI_ClockSourceCollection.cpp
+++ private_src/di/DI_ClockSourceCollection.cpp
@@ -1,3 +1,43 @@
+#include <base/container/Dictionary.h>
 #include <bsp-interface/di/clock.h>
+#include <HseClock.h>
 
-base::IDictionary<std::string, bsp::IClockSource *> DI_ClockSourceCollection();
+namespace
+{
+    class Initializer
+    {
+    public:
+        Initializer()
+        {
+            _dic.Add("hse", &bsp::HseClock::Instance());
+        }
+
+        base::Dictionary<std::string, bsp::IClockSource *> _dic{};
+    };
+} // namespace
+
+base::IDictionary<std::string, bsp::IClockSource *> const &DI_ClockSourceCollection()
+{
+    class Getter :
+        public base::SingletonGetter<Initializer>
+    {
+    public:
+        std::unique_ptr<Initializer> Create() override
+        {
+            return std::unique_ptr<Initializer>{new Initializer{}};
+        }
+
+        void Lock() override
+        {
+            DI_InterruptSwitch().DisableGlobalInterrupt();
+        }
+
+        void Unlock() override
+        {
+            DI_InterruptSwitch().EnableGlobalInterrupt();
+        }
+    };
+
+    Getter g;
+    return g.Instance()._dic;
+}
