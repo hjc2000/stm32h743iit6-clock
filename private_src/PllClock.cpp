f289c63b0e46efef709f424ca02b0a5e31966e4e
hjc2000 <hjc.666@foxmail.com>
2024-11-27 09:52:39 +0800

    PllClock

--- private_src/PllClock.cpp
+++ private_src/PllClock.cpp
@@ -5,7 +5,26 @@ std::string bsp::PllClock::Name() const
     return "pll";
 }
 
+void bsp::PllClock::Open(std::string const &input_channel_name)
+{
+    // 这里没有指定想要的输出频率，那就输出最大频率
+}
+
+void bsp::PllClock::Open(std::string const &input_channel_name,
+                         base::IDictionary<std::string, base::Hz> const &target_output_frequencies)
+{
+}
+
+void bsp::PllClock::Close()
+{
+}
+
 bsp::IClockSource_State bsp::PllClock::State() const
 {
     return _state;
+}
+
+base::Hz bsp::PllClock::Frequency() const
+{
+    return base::Hz();
 }
--- private_src/PllClock.h
+++ private_src/PllClock.h
@@ -22,19 +22,19 @@ namespace bsp
         /// @brief 打开时钟源。
         /// @param input_channel_name 输入通道名称。有的时钟源可能具有多个输入通道，例如可以从多个晶振中选择一个，
         /// 或者 PLL 可以从多个时钟源中选择一个。
-        virtual void Open(std::string const &input_channel_name);
+        void Open(std::string const &input_channel_name) override;
 
         /// @brief 打开时钟源。
         /// @param input_channel_name 输入通道名称。有的时钟源可能具有多个输入通道，例如可以从多个晶振中选择一个，
         /// 或者 PLL 可以从多个时钟源中选择一个。
         /// @param target_output_frequencies 想让本时钟源输出的目标频率列表。因为有的时钟源可能有多个输出
         /// 通道，所以就采用了字典的形式。输出通道名为 key，目标频率为 value.
-        virtual void Open(std::string const &input_channel_name,
-                          base::IDictionary<std::string, base::Hz> const &target_output_frequencies);
+        void Open(std::string const &input_channel_name,
+                  base::IDictionary<std::string, base::Hz> const &target_output_frequencies) override;
 #pragma endregion
 
         /// @brief 关闭时钟源。
-        virtual void Close() = 0;
+        void Close() override;
 
         /// @brief 本时钟源当前的状态。
         /// @return
@@ -42,6 +42,6 @@ namespace bsp
 
         /// @brief 本时钟源当前频率。
         /// @return
-        virtual base::Hz Frequency() const = 0;
+        base::Hz Frequency() const override;
     };
 } // namespace bsp
