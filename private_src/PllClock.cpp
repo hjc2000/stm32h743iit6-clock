6a76205f1131348249ac23c74c14ada20488af8d
hjc2000 <hjc.666@foxmail.com>
2024-11-27 09:39:12 +0800

    PllClock

--- private_src/PllClock.cpp
+++ private_src/PllClock.cpp
@@ -4,3 +4,8 @@ std::string bsp::PllClock::Name() const
 {
     return "pll";
 }
+
+bsp::IClockSource_State bsp::PllClock::State() const
+{
+    return _state;
+}
--- private_src/PllClock.h
+++ private_src/PllClock.h
@@ -10,6 +10,9 @@ namespace bsp
     class PllClock :
         public bsp::IClockSource
     {
+    private:
+        IClockSource_State _state = IClockSource_State::Off;
+
     public:
         /// @brief 本时钟源的名称。
         /// @return
@@ -17,13 +20,17 @@ namespace bsp
 
 #pragma region Open
         /// @brief 打开时钟源。
-        /// @param target_output_frequency 目标输出频率。
-        virtual void Open(IClockSource_TargetOutputFrequency const &target_output_frequency);
+        /// @param input_channel_name 输入通道名称。有的时钟源可能具有多个输入通道，例如可以从多个晶振中选择一个，
+        /// 或者 PLL 可以从多个时钟源中选择一个。
+        virtual void Open(std::string const &input_channel_name);
 
         /// @brief 打开时钟源。
+        /// @param input_channel_name 输入通道名称。有的时钟源可能具有多个输入通道，例如可以从多个晶振中选择一个，
+        /// 或者 PLL 可以从多个时钟源中选择一个。
         /// @param target_output_frequencies 想让本时钟源输出的目标频率列表。因为有的时钟源可能有多个输出
         /// 通道，所以就采用了字典的形式。输出通道名为 key，目标频率为 value.
-        virtual void Open(base::IDictionary<std::string, base::Hz> const &target_output_frequencies);
+        virtual void Open(std::string const &input_channel_name,
+                          base::IDictionary<std::string, base::Hz> const &target_output_frequencies);
 #pragma endregion
 
         /// @brief 关闭时钟源。
@@ -31,7 +38,7 @@ namespace bsp
 
         /// @brief 本时钟源当前的状态。
         /// @return
-        virtual IClockSource_State State() const = 0;
+        IClockSource_State State() const override;
 
         /// @brief 本时钟源当前频率。
         /// @return
