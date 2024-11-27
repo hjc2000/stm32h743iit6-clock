bd010c6bac6a1253ab91650cbe42e7fc1f3c6c25
hjc2000 <hjc.666@foxmail.com>
2024-11-27 17:19:21 +0800

    void bsp::PllClock::Open(std::string const &input_channel_name, base::IDictionary<std::string, int> const &factors)

--- private_src/PllClock.cpp
+++ private_src/PllClock.cpp
@@ -7,6 +7,55 @@ std::string bsp::PllClock::Name() const
 
 void bsp::PllClock::Open(std::string const &input_channel_name, base::IDictionary<std::string, int> const &factors)
 {
+    int m = 1;
+
+    {
+        int const *ptr = factors.Find("m");
+        if (ptr != nullptr)
+        {
+            m = *ptr;
+        }
+    }
+
+    int n = 1;
+
+    {
+        int const *ptr = factors.Find("n");
+        if (ptr != nullptr)
+        {
+            n = *ptr;
+        }
+    }
+
+    int p = 1;
+
+    {
+        int const *ptr = factors.Find("p");
+        if (ptr != nullptr)
+        {
+            p = *ptr;
+        }
+    }
+
+    int q = 1;
+
+    {
+        int const *ptr = factors.Find("q");
+        if (ptr != nullptr)
+        {
+            q = *ptr;
+        }
+    }
+
+    int r = 1;
+
+    {
+        int const *ptr = factors.Find("r");
+        if (ptr != nullptr)
+        {
+            r = *ptr;
+        }
+    }
 }
 
 void bsp::PllClock::Close()
