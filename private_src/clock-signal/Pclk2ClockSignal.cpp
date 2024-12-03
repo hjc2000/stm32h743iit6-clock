a00a1fd4aa629b748b5dd39143ea7f2005a4ef99
hjc2000 <hjc.666@foxmail.com>
2024-12-03 15:39:10 +0800

    pclk2

--- private_src/clock-signal/Pclk2ClockSignal.cpp
+++ private_src/clock-signal/Pclk2ClockSignal.cpp
@@ -36,3 +36,50 @@ base::Hz bsp::Pclk2ClockSignal::Frequency() const
     uint32_t value = HAL_RCC_GetPCLK2Freq();
     return base::Hz{value};
 }
+
+void bsp::Pclk2ClockSignal::Open(bsp::IClockSignal_InputDivisionFactor const &input_division_factor)
+{
+    RCC_ClkInitTypeDef def{};
+    def.ClockType = RCC_CLOCKTYPE_PCLK2;
+
+    switch (input_division_factor.Value())
+    {
+    case 1:
+        {
+            def.APB1CLKDivider = RCC_APB2_DIV1;
+            break;
+        }
+    case 2:
+        {
+            def.APB1CLKDivider = RCC_APB2_DIV2;
+            break;
+        }
+    case 4:
+        {
+            def.APB1CLKDivider = RCC_APB2_DIV4;
+            break;
+        }
+    case 8:
+        {
+            def.APB1CLKDivider = RCC_APB2_DIV8;
+            break;
+        }
+    case 16:
+        {
+            def.APB1CLKDivider = RCC_APB2_DIV16;
+            break;
+        }
+    default:
+        {
+            throw std::invalid_argument{"不支持此分频"};
+        }
+    }
+
+    HAL_StatusTypeDef ret = HAL_RCC_ClockConfig(&def,
+                                                FLASH_LATENCY_2);
+
+    if (ret != HAL_StatusTypeDef::HAL_OK)
+    {
+        throw std::runtime_error{"时钟信号配置失败"};
+    }
+}
