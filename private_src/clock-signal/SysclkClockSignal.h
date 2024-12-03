4b3e5571d731b7299fd06796af345678d61a9c98
hjc2000 <hjc.666@foxmail.com>
2024-12-03 16:02:16 +0800

    override

--- private_src/clock-signal/SysclkClockSignal.h
+++ private_src/clock-signal/SysclkClockSignal.h
@@ -26,6 +26,6 @@ namespace bsp
         /// @param clock_source 时钟源。像 stm32 的系统时钟 sysclk，是时钟源后的第一个时钟信号，输入端连接着
         /// 各个时钟源，输出端供给各个子时钟信号。本参数用来选择类似 sysclk 这种时钟信号的时钟源。
         virtual void Open(bsp::IClockSignal_OutputDivisionFactor const &output_division_factor,
-                          IClockSignal_ClockSource const &clock_source);
+                          IClockSignal_ClockSource const &clock_source) override;
     };
 } // namespace bsp
